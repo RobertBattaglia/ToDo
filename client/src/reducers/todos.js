bbd3a9858bd3d098b4d13741f3322dac70ac6799
Robert Battaglia <r083rt164@gmail.com>
2019-11-27 13:15:35 -0500

    add redux store and root reducer

---
+++ client/src/reducers/todos.js
@@ -0,0 +1,16 @@
+const todos = (state = {}, action) => {
+  switch (action.type) {
+    case 'CREATE_TODO':
+      break;
+    case 'COMPLETE_TODO':
+      break;
+    case 'UPDATE_TODO':
+      break;
+    case 'DELETE_TODO':
+      break;
+    default:
+      return state;
+  }
+};
+
+export default todos;
