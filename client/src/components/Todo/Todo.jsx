7d724e490f06ff26eba013aa7feb226d26ce7eb9
Robert Battaglia <r083rt164@gmail.com>
2019-11-27 13:38:42 -0500

    change webpack css modules config

--- client/src/components/Todo/Todo.jsx
+++ client/src/components/Todo/Todo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './style.css';
+import styles from './Todo.css';
 
 const Todo = () => {
   return (
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,8 +17,17 @@ const config = {
         exclude: /node_modules/
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ]
       }
     ]
   },
