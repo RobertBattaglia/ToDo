41d46a416d352dbb7cd65c2cdecd253956b8da36
Robert Battaglia <r083rt164@gmail.com>
2019-11-28 13:16:25 -0500

    add facebook login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 import Insert from './components/Insert/Insert';
 import List from './components/List/List';
 
-const App = () => (
-  <>
-    <h2>To Do</h2>
-    <Insert />
-    <List />
-  </>
-);
+const App = () => {
+  const responseFacebook = (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res);
+    }
+  };
+  return (
+    <>
+      <FacebookLogin
+        appId="423407844996823"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-facebook-button-class"
+        icon="fa-facebook"
+      />
+      <h2>To Do</h2>
+      <Insert />
+      <List />
+    </>
+  );
+};
 
 export default App;
