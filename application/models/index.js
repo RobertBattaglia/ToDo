68e597e8e1cde1b0670943233802b1db5da40048
Robert Battaglia <r083rt164@gmail.com>
2019-11-28 23:13:52 -0500

    add mongo schema and add todo route

---
+++ application/models/index.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to mongo db...');
+});
+
+const todoSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  task: String,
+  complete: Boolean
+});
+
+const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  name: String,
+  email: String,
+  todos: [todoSchema]
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports.save = async (id, name, email, todo) => {
+  const user = await User.findOne({ _id: id });
+  if (!user) {
+    new User({
+      _id: id,
+      name,
+      email,
+      todos: [todo]
+    }).save();
+  } else {
+    const todoExists = await user.todos.id(todo._id);
+    if (!todoExists) {
+      console.log(user);
+      user.todos.push(todo);
+      console.log(user);
+      user.save();
+    }
+  }
+};
+
+module.exports.find = () => {
+  return User.find();
+};
+
+module.exports.delete = () => {
+  return User.findOneAndDelete({ _id: '10219071906341489' }).exec();
+};
+
+module.exports.findTodo = () => {
+  return User.find();
+};
--- application/routes/todo.js
+++ application/routes/todo.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.post('/:hash', async (req, res) => {
-  //TODO
-  console.log('received');
-  console.log(process.env.MONGO_URI);
+const db = require('../models');
+
+router.use(express.json());
+router.get('/', async (req, res) => {
+  try {
+    console.log(await db.find());
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  const { id, name, email, todo } = req.body;
+  db.save(id, name, email, todo);
+});
+
+router.delete('/', async (req, res) => {
+  db.delete();
 });
 
 module.exports = router;
